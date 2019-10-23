952c537397a03fabae419336cc8e8f3438e6d137
Tatuana4444 <47916561+Tatuana4444@users.noreply.github.com>
2019-10-23 13:53:24 +0300

    lab5

---
+++ lab5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace lab5
+{
+    delegate void TaskDelegate();
+    class Program
+    {
+        public static void WriteHello()
+        {
+            Console.WriteLine("Hello world!");
+        }
+        static void Main(string[] args)
+        {
+            TaskDelegate[] task = new TaskDelegate[10];
+            for (int i = 0; i < 10; i++)
+                task[i] = WriteHello;
+            Parallel.WaitAll(task);
+            Console.WriteLine("ALL");
+            Console.ReadKey();
+        }
+        class Parallel
+        {
+            public static void WaitAll(TaskDelegate[] task)
+            {
+                TaskQueue taskQueue = new TaskQueue(task.Length);
+                Queue<TaskDelegate> QTask = new Queue<TaskDelegate>(task);
+                for (int i = 0; i < task.Length; i++)
+                    taskQueue.EnqueueTask(task[i]);
+                taskQueue.IsStop = true;
+                for (int i = 0; i < task.Length; i++)
+                    taskQueue.Threads[i].Join();
+            }
+
+        }
+    }
+}
---
+++ lab5/TaskQueue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace lab5
+{
+
+    class TaskQueue
+    {
+
+        static object locker = new object();
+        static Queue<TaskDelegate> QTask;
+        private Thread[] threads;
+        public Thread[] Threads
+        {
+            get { return threads; }
+           
+        }
+        private volatile bool isStop = false;
+        public bool IsStop
+        {
+            get { return isStop; }
+            set { isStop = value; }
+        }
+
+        public TaskQueue(int threadCount)//создаем пул потоков
+        {
+            QTask = new Queue<TaskDelegate>();
+            threads = new Thread[threadCount];
+            for (int i = 0; i < threadCount; i++)
+            {
+                Thread thread = new Thread(Check);
+                threads[i] = thread;
+                thread.Start();
+            }
+
+        }
+        public bool EnqueueTask(TaskDelegate task)
+        {
+            if (isStop)
+                return false;
+            QTask.Enqueue(task);
+            return true;
+        }
+
+
+        public void Check()//проверяем, есть ли в очереди задачи
+        {
+            while (!isStop || (QTask.Count > 0))
+            {//если еще не было конца или очередь не пуста
+                TaskDelegate task = null;
+                lock (locker)//блокировка, на время обращения к очереди
+                {
+                    if (QTask.Count > 0)
+                        task = QTask.Dequeue();
+                }
+                task?.Invoke();//вызов задачи 
+            }
+
+        }
+    }
+}
